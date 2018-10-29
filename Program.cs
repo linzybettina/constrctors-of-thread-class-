ee7471f32fcbb18b979c146349fa0c6e436413ca
linzybettina <43557782+linzybettina@users.noreply.github.com>
2018-10-29 21:51:56 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+    class Program
+    {
+        //static void test()
+        //{
+        //    for(int i=1;i<=100;i++)
+        //        Console.WriteLine("Test:",+i);
+        //}       
+        static void test(object max)
+        {
+            int num = Convert.ToInt32(max);  
+            for(int i=1;i<=num;i++)
+               Console.WriteLine("Test:{0}",+i);
+        }
+        static void Main(string[] args)
+        {
+         //ThreadStart obj=()=>test();
+            ParameterizedThreadStart obj = new ParameterizedThreadStart(test);
+            Thread t=new Thread(obj);
+            t.Start(76);
+            Console.ReadLine();
+        }
+    }
+
